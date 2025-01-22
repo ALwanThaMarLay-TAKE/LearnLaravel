e77f43ac1088fd9d47c3eee46ed7578449e59d9b
Thu Zin Htet <thuzinhtet.dev@gmail.com>
2025-01-22 16:06:14 +0630

    vite

--- Laracast-course/vite.config.js
+++ Laracast-course/vite.config.js
@@ -4,7 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // input: ['resources/css/app.css', 'resources/js/app.js'], //for normal server side application
+            input: ['resources/js/app.js'], //import css in js file for single page appliction like react
             refresh: true,
         }),
     ],
